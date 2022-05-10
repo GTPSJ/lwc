81f4f16deaee418fb0f5eb656d5e0946c66041b5
Gleiser Junker <101733375+undefined@users.noreply.github.com>
2022-05-10 12:33:14 +0000

    First commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+
+//criar variaveis, atributos.
+
+Nome = "Gleiser Tarine";
+Telefone = '99999-99999';
+CPF='888.888.888-88';
+Idade = 23;
+
+Email = 'gleiser@gmail.com';
+instagram = 'https://www.instagram.com/gleiser_tarine/';
+linkedin = 'https://www.linkedin.com/in/gleiser-tarine-junker-a24736221/';
+
+}
