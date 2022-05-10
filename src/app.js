35129cab77733063ea7007dde50bc468f2b6d014
Gleiser Junker <101733375+GTPSJ@users.noreply.github.com>
2022-05-10 11:54:48 -0300

    Push from copy (#1)
    Co-authored-by: Gleiser Junker <101733375+undefined@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -13,4 +13,31 @@ Email = 'gleiser@gmail.com';
 instagram = 'https://www.instagram.com/gleiser_tarine/';
 linkedin = 'https://www.linkedin.com/in/gleiser-tarine-junker-a24736221/';
 
+//comando padrão de visibilidade de modelo boleano
+visible = true;
+
+//uso da variavel da classe dentro do metodo
+//trocarDiv(){}
+//this.variavel
+trocarDiv(){
+  //metodo trocarDiv
+//this.visible = false; //!this.visible
+
+if(this.visible ==false){
+  this.visible = true;
+
+}else{
+  this.visible= false;
+  }
+}
+
+alterarValores(){
+  this.Nome="Tarine Gleiser";
+  this.Telefone="11111-11111";
+  this.CPF="111.111.111-44";
+  this.Idade= 32;
+  this.Email="tarine@gmail.com"
+ 
+}
+
 }
